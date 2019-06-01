6030962a51237096418ff81975fd5d9fd82cdbba
icor101 <riyamish16@gmail.com>
2019-06-01 16:13:22 +0530

    Completed the Module

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const fs=require('fs');
+const path=require('path');
+
+const accountsData=fs.readFileSync(
+    path.join(__dirname,'json','accounts.json'),'utf8'
+);
+const accounts=JSON.parse(accountsData);
+
+const userData=fs.readFileSync(
+    path.join(__dirname,'json','users.json'),'utf8'
+);
+const users=JSON.parse(userData);
+
+const writeJSON=()=>{
+    const accountsJSON=JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'),accountsJSON,'utf8');
+};
+
+module.exports={accounts,users,writeJSON};
